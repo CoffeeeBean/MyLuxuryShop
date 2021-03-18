8c5c30d99924780bc5047bec62d062dc738b176b
CoffeeeBean <lucia.pironi@gmail.com>
2021-03-18 12:12:10 +0100

    call api for product of the day

--- src/components/ProductOfTheDay.js
+++ src/components/ProductOfTheDay.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
-import productOfTheDayImg from '../productOfTheDay.jpg'
+// import productOfTheDayImg from '../productOfTheDay.g'
 
 
 class ProductOfTheDay extends Component{
+    state = {product: {}}
+
+    componentDidMount(){
+        fetch('http://localhost:8080/product/200')
+        .then(response => response.json()
+        .then(json => this.setState({ product: json })));
+    }
+
     render(){
+        
         return(
             <div>
                 <h2>Product Of The Day</h2>
-                <img src={productOfTheDayImg} alt='product of the day'></img>
+                <img src={this.state.product.imageUrl} alt='product of the day' className="productOfTheDay"></img>
+                <p>{this.state.product.shortDescription}</p>
             </div>
         )
     }
