157f78d891b45d8a7a093125edb4823c4dbe4bc4
CoffeeeBean <lucia.pironi@gmail.com>
2021-03-24 14:43:51 +0100

    removed obsolete header component

--- src/pages/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <div>
-      <h1> My Luxury Shop</h1>
-    </div>
-  );
-}
--- src/pages/tests/Header.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Header from "../Header";
-
-test("header with title", () => {
-  render(<Header />);
-
-  screen.getByRole("heading", { level: 1, name: "My Luxury Shop" });
-});
