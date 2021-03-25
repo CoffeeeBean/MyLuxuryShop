447e42593246ba6c80a5c4d201e57ab0efd32136
CoffeeeBean <lucia.pironi@gmail.com>
2021-03-25 16:13:16 +0100

    style for product img

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,7 +3,7 @@ import Col from "react-bootstrap/Col";
 export default function Product({ id, shortDescription, imageUrl }) {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
-      <img src={imageUrl} alt={`product_img_${id}`} />
+      <img src={imageUrl} alt={`product_img_${id}`} className="product" />
       <p>{shortDescription}</p>
     </Col>
   );
