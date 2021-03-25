8ba8e960659c133664ceaa72a565c36d71455ff5
CoffeeeBean <lucia.pironi@gmail.com>
2021-03-25 15:27:58 +0100

    fix app test for unmounted warning

--- src/components/tests/App.test.js
+++ src/components/tests/App.test.js
@@ -1,26 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { createMemoryHistory } from "history";
 import React from "react";
 import { Router } from "react-router-dom";
-
 import "@testing-library/jest-dom/extend-expect";
-
 import App from "../App";
+import axios from "axios";
+
+jest.mock("axios");
 
 test("full app rendering/navigating", async () => {
+  axios.get.mockImplementation(() => Promise.resolve({ data: [] }));
+
   const history = createMemoryHistory();
-  render(
-    <Router history={history}>
-      <App />
-    </Router>
+  await waitFor(() =>
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    )
   );
   //verify to be in HomePage
   expect(screen.getByText("Product Of The Day")).toBeInTheDocument();
 
   const leftClick = { button: 0 };
   userEvent.click(screen.getByText(/search products/i), leftClick);
-  expect(screen.getByText(/search page/i)).toBeInTheDocument();
+  expect(await screen.findByText(/search page/i)).toBeInTheDocument();
 
   userEvent.click(screen.getByText(/create new product/i), leftClick);
   expect(screen.getByText(/create product/i)).toBeInTheDocument();
